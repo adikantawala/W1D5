f7819c15d88c4906934a9b170437d8a3739ec497
adikantawala <adikan@ucla.edu>
2018-10-26 14:26:56 -0700

    Create #add_child, #remove_child, #dfs

--- PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -17,9 +17,48 @@ class PolyTreeNode
     self.value
   end
 
-  def parent=(parent_property)
-    @parent = parent_property
-    parent_property.children << self unless parent_property.nil?
+  def parent=(new_parent)
+    if self.parent.nil?
+      @parent = new_parent
+      new_parent.children << self unless new_parent.nil? || new_parent.children.include?(self)
+    else
+      if new_parent.nil?
+        self.parent.children.delete(self)
+        @parent = new_parent
+      else
+        new_parent.children << self.parent.children.delete(self)
+        @parent = new_parent
+      end
+    end
+
+    def add_child(child_node)
+      child_node.parent=(self)
+    end
+
+    def remove_child(child_node)
+      raise "Error" unless self.children.include?(child_node)
+      child_node.parent=(nil)
+    end
+
+    def dfs(target_value)
+      return self if self.value == target_value
+      return nil if self.children.empty?
+
+      self.children.each do |child|
+        temp = child.dfs(target_value)
+        return temp unless temp.nil?
+      end
+      nil
+    end
+
+    def bfs(target_value)
+      
+    end
+
+    def inspect
+      self.value
+    end
+
   end
 
 
