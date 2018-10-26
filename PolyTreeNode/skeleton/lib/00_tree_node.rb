b3857ee86387bd2612d3d87d6867c6539c0f96b3
adikantawala <adikan@ucla.edu>
2018-10-26 12:05:32 -0700

    Create #intialize, #parent, #children, #value, #parent=

--- PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,29 @@
 class PolyTreeNode
-#test
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    self.parent
+  end
+
+  def children
+    self.children
+  end
+
+  def value
+    self.value
+  end
+
+  def parent=(parent_property)
+    @parent = parent_property
+    parent_property.children << self unless parent_property.nil?
+  end
+
+
+
+  attr_reader :value, :children, :parent
+
 end
